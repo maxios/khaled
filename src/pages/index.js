2d7bcac222be2af95933d65a29181efa39e90e04
Khaled Maher <maxios.code@gmail.com>
2020-03-18 21:54:06 +0200

    add linkdedin badge in the middle of the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React, {useEffect} from "react";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>is being built</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    const element = `
+      <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="khaledmaher">
+        <a class="LI-simple-link" href='https://eg.linkedin.com/in/khaledmaher?trk=profile-badge'>
+          Khaled Maher
+        </a>
+      </div>
+    `
+
+    script.src = "https://platform.linkedin.com/badges/js/profile.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+    document.getElementById("linkedin-profile-badge").innerHTML = element;
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div id="linkedin-profile-badge" style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+      }}></div>
+    </Layout>
+  )
+}
 
 export default IndexPage
